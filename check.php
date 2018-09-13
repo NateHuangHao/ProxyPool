7c683a6a11650cdb3e9c666d1ebda68c7c9c1f6b
NateHuang <397605079@qq.com>
2018-09-13 19:59:39 +0800

    修改输出格式

--- check.php
+++ check.php
@@ -12,43 +12,37 @@ $httpClient = new Requests(['timeout' => 10]);
 $ip_pool = $redis->smembers('ip_pool');
 $ip_count = $redis->scard('ip_pool');
 
-print_r('开始检测,共有IP数目:'.$ip_count);
-echo "\n";
+echo '开始检测,共有IP数目:' . $ip_count . PHP_EOL;
 
 $success_ip = [];
 $success_count = 0;
 $error_count = 0;
 for ($i=0; $i < $ip_count; $i++) {
 	$ip = $redis->spop('ip_pool');
-	print_r('正在检测'.$ip);
-	echo "\n";
+	echo '正在检测' . $ip . PHP_EOL;
 	$response = $httpClient->test_request('GET','https://www.baidu.com', ['proxy' => 'tcp://'.$ip]);
 	if (!$response) {
-		print_r('百度网检测失败,开始检验腾讯网。。。');
-		echo "\n";
+		echo '百度网检测失败,开始检验腾讯网。。。' . PHP_EOL;
 	    $response = $httpClient->test_request('GET','http://www.qq.com', ['proxy' => 'tcp://'.$ip]);
 	    if ($response && $response->getStatusCode() == 200) {
 	    	$success_ip[] = $ip;
 	    	$success_count += 1;
-	        print_r($ip.'检验成功，重回IP池');
-			echo "\n";
-			echo "\n";
+	        echo $ip.'检验成功，重回IP池' . PHP_EOL;
+			echo PHP_EOL;
 	    }
 	    else{
 	    	$error_count += 1;
-	        print_r($ip.'检验失败，移出IP池');
-	        echo "\n";
-	        echo "\n";
+	        echo $ip.'检验失败，移出IP池' . PHP_EOL;
+	        echo PHP_EOL;
 	    }
 	}
 	else if ($response->getStatusCode() == 200){
-		print_r('百度网检测成功!!!');
+		echo '百度网检测成功!!!');
 		echo "\n";
 	    $success_ip[] = $ip;
 	    $success_count += 1;
-		print_r($ip.'检验成功，重回IP池');
-		echo "\n";
-		echo "\n";
+		echo $ip.'检验成功，重回IP池' . PHP_EOL;
+		echo PHP_EOL;
 	}
 }
 
@@ -56,5 +50,5 @@ foreach ($success_ip as $key => $value) {
 	$redis->sadd('ip_pool',$value);
 }
 
-print_r('检查完成!! 检测失败IP数:'.$error_count.",检测成功IP数:".$success_count);
-echo "\n";
+echo '检查完成!! 检测失败IP数:'.$error_count.",检测成功IP数:".$success_count . PHP_EOL;
+echo PHP_EOL;
